72f7c9c4dcbc100e583316bc2c6efc25a30ff736
jevazquezb <je.vazquezb@gmail.com>
2022-06-07 09:22:33 -0500

    Add code for reviewing exercise

---
+++ src/modules/task_manager.js
@@ -0,0 +1,32 @@
+import { getTasks, saveTasks } from './local_storage.js';
+import Task from './task_class.js';
+
+function addTask(description, list) {
+  const taskId = list.length + 1;
+  const newTask = new Task(description, taskId);
+  list.push(newTask);
+  return [newTask, list];
+}
+
+function removeFromStorage(e) {
+  const tasksList = getTasks();
+  const taskId = +e.target.id.slice(5);
+  const filteredTasks = tasksList.filter((task) => task.index !== taskId);
+  for (let i = 0; i < filteredTasks.length; i += 1) {
+    filteredTasks[i].index = i + 1;
+  }
+  saveTasks(filteredTasks);
+}
+
+function editDescription(e) {
+  const tasksList = getTasks();
+  const taskId = +e.target.id.slice(4);
+  tasksList.forEach((task) => {
+    if (taskId === task.index) {
+      task.description = e.target.value;
+    }
+  });
+  saveTasks(tasksList);
+}
+
+export { addTask, removeFromStorage, editDescription };
